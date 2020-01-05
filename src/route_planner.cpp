b922d6bc235f13f73c07cb708b655d68170a42bd
alice <bow1226@gmail.com>
2020-01-05 08:10:53 +0800

    fix: Use break tatement terminates the switch or loop, and execution continues at the first statement beyond the switch or loop.

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -109,7 +109,7 @@ void RoutePlanner::AStarSearch()
         if (current_node->distance(*end_node) == 0)
         {
             m_Model.path = ConstructFinalPath(current_node);
-            return;
+            break;
         }
         AddNeighbors(current_node);
     }
